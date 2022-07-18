f7de3fa8a7ba645a9a40d6890c844fdb4bea12a0
Oliver Dixon <olly257@gmail.com>
2022-07-18 14:50:26 +0200

    Added logger options, disable and enable

--- lib/src/common/logger.dart
+++ lib/src/common/logger.dart
@@ -2,6 +2,12 @@ const String APP_NAME = 'mediasoup-client';
 
 typedef void LoggerDebug(dynamic message);
 
+bool isLogging = true;
+
+setLoggerStatus(bool isLoggingP) {
+  isLogging = isLogging;
+}
+
 class Logger {
   final String? _prefix;
 
@@ -10,26 +16,28 @@ class Logger {
   late LoggerDebug error;
 
   Logger(this._prefix) {
-    if (_prefix != null) {
-      debug = (dynamic message) {
-        print('$APP_NAME:$_prefix $message');
-      };
-      warn = (dynamic message) {
-        print('$APP_NAME:WARN:$_prefix $message');
-      };
-      error = (dynamic message) {
-        print('$APP_NAME:ERROR:$_prefix $message');
-      };
-    } else {
-      debug = (dynamic message) {
-        print('$APP_NAME $message');
-      };
-      warn = (dynamic message) {
-        print('$APP_NAME:WARN $message');
-      };
-      error = (dynamic message) {
-        print('$APP_NAME:ERROR $message');
-      };
+    if (isLogging) {
+      if (_prefix != null) {
+        debug = (dynamic message) {
+          print('$APP_NAME:$_prefix $message');
+        };
+        warn = (dynamic message) {
+          print('$APP_NAME:WARN:$_prefix $message');
+        };
+        error = (dynamic message) {
+          print('$APP_NAME:ERROR:$_prefix $message');
+        };
+      } else {
+        debug = (dynamic message) {
+          print('$APP_NAME $message');
+        };
+        warn = (dynamic message) {
+          print('$APP_NAME:WARN $message');
+        };
+        error = (dynamic message) {
+          print('$APP_NAME:ERROR $message');
+        };
+      }
     }
   }
 }
